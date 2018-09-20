fc8c9ef65662c04e9aeaa3bc190fa2a7db01ea86
Julia <julia.grasevych@gmail.com>
2018-09-20 12:10:52 +0300

    Using Swift 4 key paths for autolayout

--- ImageFlickrSearch/ImageFlickrSearch/Extensions/NSLayoutConstraint+Utils.swift
+++ ImageFlickrSearch/ImageFlickrSearch/Extensions/NSLayoutConstraint+Utils.swift
@@ -15,10 +15,10 @@ extension NSLayoutConstraint {
             return
         }
         childView.translatesAutoresizingMaskIntoConstraints = false
-        childView.topAnchor.constraint(equalTo: parentView.topAnchor).isActive = true
-        childView.bottomAnchor.constraint(equalTo: parentView.bottomAnchor).isActive = true
-        childView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor).isActive = true
-        childView.trailingAnchor.constraint(equalTo: parentView.trailingAnchor).isActive = true
+        childView.anchor(to: parentView, at: \UIView.topAnchor)
+        childView.anchor(to: parentView, at: \UIView.bottomAnchor)
+        childView.anchor(to: parentView, at: \UIView.leadingAnchor)
+        childView.anchor(to: parentView, at: \UIView.trailingAnchor)
     }
     static func centerInParent(childView: UIView) {
         guard let parentView = childView.superview else {
@@ -26,7 +26,13 @@ extension NSLayoutConstraint {
             return
         }
         childView.translatesAutoresizingMaskIntoConstraints = false
-        childView.centerXAnchor.constraint(equalTo: parentView.centerXAnchor).isActive = true
-        childView.centerYAnchor.constraint(equalTo: parentView.centerYAnchor).isActive = true
+        childView.anchor(to: parentView, at: \UIView.centerXAnchor)
+        childView.anchor(to: parentView, at: \UIView.centerYAnchor)
+    }
+}
+
+extension UIView {
+    func anchor<Anchor, AnchorType>(to view: UIView, at anchor: KeyPath<UIView, Anchor>) where Anchor: NSLayoutAnchor<AnchorType> {
+        self[keyPath: anchor].constraint(equalTo: view[keyPath: anchor]).isActive = true
     }
 }
